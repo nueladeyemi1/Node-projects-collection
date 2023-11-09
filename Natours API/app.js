a4bc2eea5e6905b880a9bd9b0fab314f94da0bd3
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-11-09 02:39:34 +0100

    refractor

--- Natours API/app.js
+++ Natours API/app.js
@@ -1,76 +1,37 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+app.use((req, res, next) => {
+  console.log('hello world');
 
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
+  next();
 });
 
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          newTour
-        }
-      });
-    }
-  );
+  next();
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  // console.log(req.params.id);
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
 
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-});
+//ROUTES
 
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  }
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<>Updated tour here...</>'
-    }
-  });
-});
-
-const port = 2000;
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
