9a1584126792e0c0a2538d09802e04d42e7d985b
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-11-15 23:45:40 +0100

    pagination

--- Natours API/controllers/tourController.js
+++ Natours API/controllers/tourController.js
@@ -1,3 +1,4 @@
+const { x } = require('tar');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
@@ -22,6 +23,29 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    //FIELD
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //PAGINATION
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // SECOND WAY OF QUERY DATA IN MONGOOSE
