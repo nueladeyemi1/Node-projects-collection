ed64a73c554235bf8b89fe4432d4522f7e387227
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-15 23:54:26 +0100

    auth, security and authorization comp

--- Natours API/models/userModel.js
+++ Natours API/models/userModel.js
@@ -45,7 +45,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
@@ -54,6 +59,12 @@ userSchema.pre('save', async function(next) {
 
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
+
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } });
 
   next();
 });
