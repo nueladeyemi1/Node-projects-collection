920f7c0b4750df03e8b01b9f0c04fa2060907433
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-17 15:44:03 +0100

    data modelling --

--- Natours API/models/tourModel.js
+++ Natours API/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-// const slugify = require('slugify');
+const slugify = require('slugify"');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -30,7 +31,7 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: ['easy', 'medium', 'difficulty'],
+        values: ['easy', 'medium', 'difficult'],
         message: 'Difficulty is either: easy, medium, difficulty'
       }
     },
@@ -81,7 +82,38 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      //GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    // guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtual: true },
@@ -95,7 +127,18 @@ const tourSchema = new mongoose.Schema(
 
 // Document Middleware: runs on .save() and .create()
 
-// tourSchema.pre('save', function() {});
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+
+  next();
+});
 
 tourSchema.pre(/^find/, function(next) {
   this.find({
@@ -109,6 +152,15 @@ tourSchema.pre(/^find/, function(next) {
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   console.log(docs);
+
+  next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
 
   next();
 });
