67d8dc69436f0c01159590890f73a9114e962d19
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-18 12:35:24 +0100

    data review --

---
+++ Natours API/controllers/reviewController.js
@@ -0,0 +1,30 @@
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  if (!reviews) {
+    return next(new AppError('No reviews yet'), 400);
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews
+    }
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview
+    }
+  });
+});
