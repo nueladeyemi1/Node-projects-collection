96f5e9875f0afee445023e75b7c625ec78a32a95
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-02-22 16:22:43 +0100

    socket.io --messages

--- KoppaKitsExhange API/app.js
+++ KoppaKitsExhange API/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const itemRoutes = require('./src/routes/itemRoutes')
 const userRoutes = require('./src/routes/userRoutes')
@@ -8,9 +11,8 @@ const userRoutes = require('./src/routes/userRoutes')
 require('dotenv').config()
 require('./DB.js')
 
-const app = express()
-
 app.use(cors())
+app.use('*', cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,9 +28,13 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/items', itemRoutes)
-
+// app.use('/api/v1/messages', messageRoutes)
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+  console.log('Socket.io is connected')
+})
+
+http.listen(PORT, () => {
   console.log(`Listening to ${PORT}`)
 })
--- KoppaKitsExhange API/src/routes/userRoutes.js
+++ KoppaKitsExhange API/src/routes/userRoutes.js
@@ -1,17 +1,70 @@
 const express = require('express')
 const { signup, login } = require('../controllers/userController')
+const Message = require('../models/Message')
+const User = require('../models/User')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const users = await User.find({})
+
   res.status(200).json({
     status: 'success',
-    message: 'It is working',
+    data: users,
   })
 })
+
+router
+  .route('/:userId/messages')
+  .post(async (req, res) => {
+    try {
+      // const messages = new Message({ name: req.params.userId, ...req.body })
+
+      // await messages.save(() => {
+      //   io.emit('message', req.body)
+      // })
+
+      // console.log(req.body)
+
+      const messages = await Message.create({
+        name: req.params.userId,
+        ...req.body,
+      })
+
+      io.emit('message', messages)
+
+      res.status(200).json({
+        status: 'success',
+        messages: messages,
+      })
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+  .get(async (req, res) => {
+    try {
+      const messages = await Message.find({
+        name: req.params.userId,
+      }).populate()
+
+      res.status(200).json({
+        status: 'success',
+        messages: messages,
+      })
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
 
 router.post('/signup', signup)
 
 router.post('/login', login)
+
+io.on('connection', () => {
+  console.log('socket.oi is connected')
+})
 
 module.exports = router
